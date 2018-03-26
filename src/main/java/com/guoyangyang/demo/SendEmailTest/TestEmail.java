868daba724ff25fae63472738b191d24d9d8ca85
guoyyit <guoyyit@163.com>
2018-03-26 15:14:17 +0800

    修改

---
+++ src/main/java/com/guoyangyang/demo/SendEmailTest/TestEmail.java
@@ -0,0 +1,14 @@
+package com.guoyangyang.demo.SendEmailTest;
+
+/**
+ * @author guoyangyang
+ * Date: 2018/3/26
+ * Time: 15:12
+ */
+public class TestEmail {
+
+    public static void main(String[] args) {
+        System.out.println("hello Word!");
+    }
+
+}
